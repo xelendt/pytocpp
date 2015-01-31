2511891d00a2a791348d62be02f3914446c48220
xelendt <max@user3-239-26.wireless.utoronto.ca>
2015-01-31 17:51:21 -0500

    About to finish if statements

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 #import math
 
+a = b = 1.5
+
 a = 5**1
 a = (4+3)+ 4
 a = 5
 a = (-a + 5)
+
+if ( a > 5):
+	a = 3
+elif (b == a ):
+	b += 2
+else:
+	b = 2
+
+a = a > b < a < b > a == b
+
+c = ((2 - 1) * 4) * (b + a) + 6 		
+
+a = True
+b = False
+
+c = a or b
 
 #for i in range(1, 10):
 #	print i
--- walkPython.py
+++ walkPython.py
@@ -3,11 +3,14 @@ import sys, ast
 depth = 0
 cppcode = ["#include <cmath>",
 		  	"#include <iostream>",
-			"#include <cstdlib>"]
+			"#include <cstdlib>",
+			"#include <string>",
+			"using namespace std;"]
 
 parents = []
 
-literals = ['Num', 'Str', 'Bytes', 'Name']
+literals = ['Num', 'Str', 'Bytes', 'Name', 'NameConstant', 'float', 'int']
+variables = []
 
 def tokenize(node):
 	if (type(node).__name__ == 'Num'):
@@ -15,14 +18,121 @@ def tokenize(node):
 	elif (type(node).__name__ == 'Str' or type(node).__name__ == 'Bytes'):
 		return node.s
 	elif (type(node).__name__ == 'Name'):
-		print node.id
-		return node.id
+		if (node.id == 'True'):
+			return 'true'
+		elif (node.id == 'False'):
+			return 'false'
+		else:
+			return node.id
 
+def tokenizeNode(node):
+	a = []
+	global literals, depth
+	if (type(node).__name__ in literals):
+		a.append(tokenize(node))
+	else:
+		depth += 1
+		a.append(NodeWalker().generic_visit(node))
+	return a
+
+def get_Type(node):
+	if (type(node).__name__ in literals):
+		if (type(node).__name__ == 'Num'):
+			return type(node.n)
+		else:
+			return type(node).__name__
+	elif (type(node).__name__ == 'Compare'):
+		return 'NameConstant'
+	elif (type(node).__name__ == 'UnaryOp'):
+		return get_Type(node.operand)
+	elif (type(node).__name__ == 'BinOp'):
+		left = get_Type(node.left)
+		right = get_Type(node.right)
+		if left == 'Str' or left == 'NameConstant':
+			return left
+		elif (get_Type(type(node.left)) == "float" or get_Type(type(node.right)) == "float"):
+			return type(1.2) #float
+		else:
+		 	return type(1) #int
+
+def visit_Assign(node):
+	''' TODO
+		- double pointers
+		- arrays
+		- commas (a, b = 1)
+	'''
+	global depth, variables
+	a = []	
+
+	variableType = get_Type(node.value)
+
+	for t in node.targets:
+		if (type(t).__name__ == 'Name'):
+			if t.id not in variables:
+				if (variableType.__name__ == 'NameConstant'):
+					a.append("bool " + str(t.id) + ";\n")
+				elif (variableType.__name__ == 'Str'):
+					a.append("string " + str(t.id) + ";\n")
+				elif (variableType.__name__ == 'float'):
+					a.append("double " + str(t.id) + ";\n")
+				elif (variableType.__name__ == 'int'):
+					a.append("int " + str(t.id) + ";")
+				variables.append(t.id)
+		else:	
+			if t.id not in variables:
+				if (variableType.__name__ == 'NameConstant'):
+					a.append("bool* " + str(t.id) + ";\n")
+				elif (variableType.__name__ == 'Str'):
+					a.append("string* " + str(t.id) + ";\n")
+				elif (variableType.__name__ == 'float'):
+					a.append("double* " + str(t.id) + ";\n")
+				elif (variableType.__name__ == 'int'):
+					a.append("int* " + str(t.id) + ";")
+				variables.append(t.id)
+
+	for t in node.targets:
+		a.append(t.id + ' = ' )
+	if (type(node.value).__name__ in literals):
+		if ((type(node.value).__name__ == 'Num')):
+			a.append(node.value.n)
+		elif (node.value.id == 'True'):
+			a.append("true")
+		elif (node.value.id == "False"):
+			a.append("false")
+		else:
+			a.append(node.value.s)
+	else:
+		depth += 1
+		a.append(NodeWalker().generic_visit(node.value))
+#	a.append(";")	
+
+	return a
+				
+
+def visit_BoolOp(node):
+	a = []
+	global depth
+	for t in node.values:
+		print "Node name", type(t).__name__
+		if (type(t).__name__ in literals):
+			a.append(tokenize(t))
+		else:
+			depth += 1
+			a.append(generic_visit(t))
+	
+	print node.op
+	if (type(node.op).__name__ == 'And'):
+		a = a[0] + "&&" + a[1]
+	elif (type(node.op).__name__ == 'Or'):
+		a = a[0] + '||' + a[1]
+	return a
+		
 def visit_BinOp(node):
 	global depth
 	''' TODO 
 		- matmult
 		- floor div
+		- brackets for recursive calls
 	'''
 	a = []
 	''' left side '''
@@ -61,6 +171,78 @@ def visit_BinOp(node):
 	elif (type(node.op).__name__ == 'Pow'):
 		a.insert(0, 'Pow(')
 		a.insert(len(a), ')')
+
+	return a
+
+def visit_If(node, state):
+	global depth
+	a = []
+	if (state == 'if'):
+		a.append('if(')
+		depth += 1
+		a.append(NodeWalker().generic_visit(node.test))
+		a.append(')')
+	elif (state == 'elif'):
+		a.append('else if(')
+		depth += 1	
+		a.append(NodeWalker().generic_visit(node.test))
+		a.append(')')
+	else:
+		a.append('else')
+	a.append('{')
+	for t in node.body:
+		a.append(tokenizeNode(t))
+	if (type(node.orelse).__name__ == 'elif'):#node.orelse != []):
+		a.append(visit_If(node.orelse, 'elif'))
+	elif(type(node.orelse)).__name__ == 'else'):
+		a.append(visit_If(node.orelse, 'else'))
+	else:
+		a.append('}')
+		return a
+
+def visit_Comparison(node):
+	a = []
+	global depth
+	''' we take in the 'left' '''
+
+	a.append(tokenizeNode(node.left))
+	if (type(node.ops[0]).__name__ == 'Eq'):
+		a.append('==')
+	elif (type(node.ops[0]).__name__ == 'NotEq'):
+		a.append('!=')
+	elif (type(node.ops[0]).__name__ == 'Lt'):
+		a.append('<')
+	elif (type(node.ops[0]).__name__ == 'LtE'):
+		a.append('<=')
+	elif (type(node.ops[0]).__name__ == 'Gt'):
+		a.append('>')
+	elif (type(node.ops[0]).__name__ == 'GtE'):
+		a.append('>=')
+	a.append(tokenizeNode(node.comparators[0]))
+
+	for i in range (0,len(node.ops)-1):
+		a.append("&&")
+		a.append(tokenizeNode(node.comparators[i]))
+		if (type(node.ops[i+1]).__name__ == 'Eq'):
+			a.append('==')
+		elif (type(node.ops[i+1]).__name__ == 'NotEq'):
+			a.append('!=')
+		elif (type(node.ops[i+1]).__name__ == 'Lt'):
+			a.append('<')
+		elif (type(node.ops[i+1]).__name__ == 'LtE'):
+			a.append('<=')
+		elif (type(node.ops[i+1]).__name__ == 'Gt'):
+			a.append('>')
+		elif (type(node.ops[i+1]).__name__ == 'GtE'):
+			a.append('>=')
+#	elif (type(node.ops[i]).__name__ == 'Is'):
+#			a.append('==')
+		a.append(tokenizeNode(node.comparators[i+1]))
+
+		
+
+	''' for a < b < c < d < e, you have to do a == b && b == c && c == d etc ... '''
+
 
 	return a
 
@@ -112,12 +294,22 @@ class NodeWalker(ast.NodeVisitor):
 			print a
 		elif (type(node).__name__ == 'UnaryOp'):
 			a += visit_UnaryOp(node)
+		elif (type(node).__name__ == 'Assign'):
+			a += visit_Assign(node)
+		elif (type(node).__name__ == 'BoolOp'):
+			a += visit_BoolOp(node)
+		elif (type(node).__name__ == 'Compare'):
+			a += visit_Comparison(node)
+		elif (type(node).__name__ == 'If'):
+			a += visit_If(node, 'if')
 		else:
 			self.visitChildren(node)
 
 		if (depth == 0):
-			cppcode += a
+			cppcode += a + [';\n']
 		else:
+#	a.append(')')
+#		 	a = ['('] + a[:len(a)]
 		 	depth -=1
 		return a
 
@@ -128,3 +320,5 @@ print ast.dump(pycode)
 #NodeWalker().visit(pycode)
 cppcode += NodeWalker().generic_visit(pycode)
 print cppcode
+for elmt in cppcode:
+	print elmt
